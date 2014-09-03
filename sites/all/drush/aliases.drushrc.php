82d574049b862c3f3b5114cf61905d8ad376acf9
Chris Morris <cmorris@ccistudios.com>
2014-09-03 14:34:05 -0400

    Added drush alias for staging site

---
+++ sites/all/drush/aliases.drushrc.php
@@ -0,0 +1,12 @@
+<?php
+
+$aliases['dev'] = array(
+	'uri'=> 'dev.notredameoffcampushousing.com',
+	'root' => '/home/ndhous/subdomains/dev/public_html',
+	'remote-host'=> 'host.lambtonshield.com',
+	'remote-user'=> 'ndhous',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	)
+);
+
