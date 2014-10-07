5ed1d8427f10dd31aafcaeb086d01b6e72034a15
Chris Morris <cmorris@ccistudios.com>
2014-10-07 09:37:53 -0400

    Update drush alias with new server name

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -3,7 +3,7 @@
 $aliases['dev'] = array(
 	'uri'=> 'dev.notredameoffcampushousing.com',
 	'root' => '/home/ndhous/subdomains/dev/public_html',
-	'remote-host'=> 'host.lambtonshield.com',
+	'remote-host'=> 'host.cciserver2.com',
 	'remote-user'=> 'ndhous',
 	'path-aliases'=> array(
 		'%files'=> 'sites/default/files',
